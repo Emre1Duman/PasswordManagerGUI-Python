499005ef09b35364606b78b46fbe710b43a7d606
Emre Duman <76225090+Emre1Duman@users.noreply.github.com>
2023-10-24 10:55:28 +0100

    Updated UI & created function to save account details to txt

--- main.py
+++ main.py
@@ -4,51 +4,54 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+def save():
+    new_website = website_entry.get()
+    new_username = username_entry.get()
+    new_password = password_entry.get()
+    f = open("data.txt", "a")
+    f.write(f"\n{new_website}| {new_username} | {new_password}")
+    f.close()
+
+
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
+#Canvas
 canvas = Canvas(width=200, height=200)
 img = PhotoImage(file="logo.png")
-canvas.create_image(100, 100, image=img)
+canvas.create_image(122, 90, image=img)
 canvas.grid(column=1, row=0)
 
+#Labels
 website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
-
 username_label = Label(text="Email/Username:")
 username_label.grid(column=0, row=2)
-
 password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
 
+
+#Entries
 website_entry = Entry(width=35)
+website_entry.focus()
 website_entry.grid(column=1, row=1, columnspan=2)
-
 username_entry = Entry(width=35)
+username_entry.insert(END, string="uzumaki@gmail.com")
 username_entry.grid(column=1, row=2, columnspan=2)
+password_entry = Entry(width=35)
+password_entry.grid(column=1, row=3, columnspan=2)
 
-password_entry = Entry(width=21)
-password_entry.grid(column=1, row=3)
 
+#Buttons
 password_button = Button(text="Generate Password")
 password_button.grid(column=2, row=3)
-
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=34, command=save)
 add_button.grid(column=1, row=4, columnspan=2)
-
-
-
-
-
-
-
-
-
-
-
 
 
 window.mainloop()
